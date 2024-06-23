5b2b8e2064fc43fbb94ff0ca10bbeecfaf1082c9
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-06-23 16:53:00 +0300

    ---

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,6 +1,7 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.http.NotFoundResponse;
 
 public class HelloWorld {
     public static void main(String[] args) {
@@ -15,6 +16,17 @@ public class HelloWorld {
                 name = "World";
             }
             ctx.result("Hello, " + name + "!");
+        });
+        app.get("/courses/{courseId}/lessons/{id}", ctx -> {
+            var courseId = ctx.pathParam("courseId");
+            var lessonId =  ctx.pathParam("id");
+            if (lessonId.equals("z")) {
+                throw  new NotFoundResponse("Entity with id = " + lessonId + " not found");
+            }
+            ctx.result("Course ID: " + courseId + " Lesson ID: " + lessonId);
+        });
+        app.get("/users/{id}", ctx -> {
+            ctx.result("User ID: " + ctx.pathParam("id"));
         });
 
         app.start(7070);
